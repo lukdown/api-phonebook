82996aa2b09bb914056cc0260dbdc198d8c82dd3
seed-yoo <lukdown10@naver.com>
2024-04-22 10:40:23 +0900

    .

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -20,6 +20,7 @@ public class PhonebookController {
 	@Autowired
 	private PhonebookService phonebookService;
 
+	
 	// 삭제
 	@DeleteMapping(value = "/api/phonebooks")
 	public String remove(@RequestBody PersonVo personVo) {
